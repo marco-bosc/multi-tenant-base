f69249cb837908389bfe7d2d8aab652aa874c5d4
marco-bosc <ktm125.6days@gmail.com>
2025-03-03 10:52:55 +0000

    Multi-tenant base

---
+++ my-saas/src/context/provider.tsx
@@ -0,0 +1,45 @@
+import { forwardRef } from 'react'
+import { LinkProps, SaasProvider } from '@saas-ui/react'
+import { RouterProvider, createRouter, Link } from '@tanstack/react-router'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+
+import { routeTree } from '../routeTree.gen'
+
+const queryClient = new QueryClient()
+
+// Set up a Router instance
+const router = createRouter({
+  routeTree,
+  context: {
+    queryClient,
+  },
+  defaultPreload: 'intent',
+  // Since we're using React Query, we don't want loader calls to ever be stale
+  // This will ensure that the loader is always called when the route is preloaded or visited
+  defaultPreloadStaleTime: 0,
+})
+
+// Register things for typesafety
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router
+  }
+}
+
+// This makes sure Saas UI components use our router
+const LinkComponent = forwardRef<HTMLAnchorElement, Pick<LinkProps, 'href'>>(
+  (props, ref) => {
+    const { href, ...rest } = props
+    return <Link ref={ref} to={href} {...rest} />
+  }
+)
+
+export const Provider = () => {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <SaasProvider linkComponent={LinkComponent}>
+        <RouterProvider router={router} />
+      </SaasProvider>
+    </QueryClientProvider>
+  )
+}
---
+++ my-saas/src/routes/index.tsx
@@ -0,0 +1,35 @@
+import { Box, Heading } from '@chakra-ui/react'
+import { createFileRoute, redirect } from '@tanstack/react-router'
+
+export const Route = createFileRoute('/')({
+  component: Index,
+  beforeLoad: async () => {
+    // We will add authentication here later
+    const user = {
+      id: '123',
+      email: 'john.doe@acme.com',
+    }
+
+    if (!user) {
+      return
+    }
+
+    const workspace = localStorage.getItem('workspace')
+
+    const path = workspace ? `/${workspace}` : '/getting-started'
+
+    throw redirect({
+      to: path,
+    })
+  },
+})
+
+function Index() {
+  return (
+    <Box p="8">
+      <Heading as="h1" mb="4">
+        Welcome Home!
+      </Heading>
+    </Box>
+  )
+}
